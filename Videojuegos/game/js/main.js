24f8cdaaddfcd7c27caf521dde088bdd3eac3426
Santiago Cordova <A01029211@tec.mx>
2025-03-20 08:33:21 -0600

    hice la pagine donde el usuario hara el log in y al momento de picarle al boton te manda a la pagina principal. Por ahora la pagina solo funciona con el usuario hola y con la contraseña 1234

---
+++ Videojuegos/game/js/main.js
@@ -0,0 +1,17 @@
+const canvasWidth = 1000;
+const canvasHeight = 600;
+
+function main() {
+    // Get a reference to the object with id 'canvas' in the page
+    const canvas = document.getElementById('canvas');
+    // Resize the element
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    // Get the context for drawing in 2D
+    ctx = canvas.getContext('2d');
+
+    
+    drawScene(0);
+
+    
+}
---
+++ Videojuegos/game/js/paginaInicial/funciones/login.js
@@ -0,0 +1,18 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username && password) {
+        console.log("Usuario:", username, "Contraseña:", password);
+
+        // Lógica de validación del login (simulada aquí con un ejemplo)
+        if (username === "hola" && password === "1234") {
+            // Si los datos son correctos, redirige a la página principal
+            window.location.href = 'mainpage.html';
+        } else {
+            alert("Usuario o contraseña incorrectos.");
+        }
+    } else {
+        alert("Por favor, ingresa un usuario y contraseña.");
+    }
+}
