ff1d9f798ca291be428d1f5a05fbc30e82ed25e7
Kvzito <blondednal@gmail.com>
2025-04-27 17:22:45 -0600

    Modifico boton de jugar en 2 htmls, además agrego íconos para reducir las columnas que usa la tabla del global ranking

--- Infinity_Jump/files/infinity_jump_db.sql
+++ Infinity_Jump/files/infinity_jump_db.sql
@@ -47,50 +47,43 @@ INSERT INTO Usuarios (usuario, contrasena) VALUES
 ('jugador9', 'secure555'),
 ('jugador10', 'mypass777');
 
+
 -- Insertar partidas dummie
-INSERT INTO Partidas (id_usuario, intento, nivel, plataformas_alcanzadas) VALUES
-(1, 1, 1, 5),
-(1, 2, 2, 79),
-(2, 1, 1, 8),
-(2, 2, 1, 5),
-(2, 3, 1, 10),
-(2, 4, 1, 20),
-(2, 5, 1, 40),
-(2, 6, 1, 80),
-(2, 7, 1, 160),
-(2, 8, 1, 320),
-(2, 9, 1, 640),
-(3, 1, 1, 10),
-(3, 2, 2, 12),
-(4, 1, 2, 13),
-(5, 1, 2, 85),
-(5, 2, 1, 16),
-(6, 1, 1, 18),
-(7, 1, 1, 19),
-(8, 1, 1, 20),
-(9, 1, 1, 22),
-(10, 1, 1, 24);
-
-INSERT INTO Partidas (id_usuario, intento, nivel, plataformas_alcanzadas) VALUES
-(11, 4, 3, 990);
-
+INSERT INTO Partidas (id_usuario, intento, nivel, plataformas_alcanzadas, mejora_1, mejora_2, mejora_3) VALUES
+(1, 1, 1, 5, 2, 5, 1),
+(1, 2, 2, 79, 2, 5, 1),
+(2, 1, 1, 8, 2, 5, 1),
+(2, 2, 1, 5, 2, 5, 0),
+(2, 3, 1, 10, 2, 5, 1),
+(2, 4, 1, 20, 2, 5, 1),
+(2, 5, 1, 40, 2, 5, 1),
+(2, 6, 1, 80, 2, 5, 1),
+(2, 7, 1, 160, 2, 0, 1),
+(2, 8, 1, 320, 2, 5, 1),
+(2, 9, 1, 640, 2, 5, 1),
+(3, 1, 1, 10, 2, 0, 1),
+(3, 2, 2, 12, 2, 5, 1),
+(4, 1, 2, 13, 2, 5, 1),
+(5, 1, 2, 85, 0, 5, 1),
+(5, 2, 1, 16, 2, 5, 1),
+(6, 1, 1, 18, 2, 5, 1),
+(7, 1, 1, 19, 2, 5, 1),
+(8, 1, 1, 20, 0, 5, 1),
+(9, 1, 1, 22, 2, 0, 0),
+(10, 1, 1, 24, 2, 5, 1);
 
 -- Insertar inventarios dummie
 INSERT INTO Inventario (id_usuario, cantidad_mejora_1, cantidad_mejora_2, cantidad_mejora_3) VALUES
-(1, 0, 1, 0),
-(2, 0, 0, 0),
-(3, 0, 0, 0),
-(4, 0, 0, 0),
+(1, 3, 1, 0),
+(2, 1, 0, 5),
+(3, 1, 1, 1),
+(4, 0, 0, 3),
 (5, 0, 0, 1),
-(6, 0, 0, 0),
-(7, 0, 0, 0),
+(6, 4, 0, 3),
+(7, 1, 0, 0),
 (8, 0, 0, 0),
-(9, 0, 0, 0),
-(10, 0, 0, 0);
-
-UPDATE Inventario
-SET cantidad_mejora_1 = 3
-WHERE id_usuario = 2;
+(9, 0, 3, 0),
+(10, 5, 0, 0);
 
 
 
